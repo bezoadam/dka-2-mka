834bc1bc35e4fe487c5adff9285d89dba0b22f84
Adam Bezak <bezoadam95@gmail.com>
2018-02-18 20:28:39 +0100

    init argument parsing from docs example

--- dka-2-mka.hs
+++ dka-2-mka.hs
@@ -1,10 +1,64 @@
 --DKA-2-MKA
 --Adam Bezák xbezak01
 
-
+import Control.Monad.State
+import qualified Data.IntMap as M
+import Data.Word
 import System.Environment
+import System.Exit
+import System.Console.GetOpt
+import Data.Maybe ( fromMaybe )
 
--- print "jebe"
+data Options = Options
+	{ optVerbose     :: Bool
+	, optShowVersion :: Bool
+	, optOutput      :: Maybe FilePath
+	, optInput       :: Maybe FilePath
+	, optLibDirs     :: [FilePath]
+	} deriving Show
+
+defaultOptions    = Options
+	{ optVerbose     = False
+	, optShowVersion = False
+	, optOutput      = Nothing
+	, optInput       = Nothing
+	, optLibDirs     = []
+	}
+
+options :: [OptDescr (Options -> Options)]
+options =
+	[ Option ['v']     ["verbose"]
+	    (NoArg (\ opts -> opts { optVerbose = True }))
+	    "chatty output on stderr"
+	, Option ['V','?'] ["version"]
+	    (NoArg (\ opts -> opts { optShowVersion = True }))
+	    "show version number"
+	, Option ['o']     ["output"]
+	    (OptArg ((\ f opts -> opts { optOutput = Just f }) . fromMaybe "output")
+	            "FILE")
+	    "output FILE"
+	, Option ['c']     []
+	    (OptArg ((\ f opts -> opts { optInput = Just f }) . fromMaybe "input")
+	            "FILE")
+	    "input FILE"
+	, Option ['L']     ["libdir"]
+	    (ReqArg (\ d opts -> opts { optLibDirs = optLibDirs opts ++ [d] }) "DIR")
+	    "library directory"
+	]
+
+compilerOpts :: [String] -> IO (Options, [String])
+compilerOpts argv =
+  case getOpt Permute options argv of
+     (o,n,[]  ) -> return (foldl (flip id) defaultOptions o, n)
+     (_,_,errs) -> ioError (userError (concat errs ++ usageInfo header options))
+ where header = "Usage: ic [OPTION...] files..."
+
 main = do
-	arg <- getArgs
-	print arg
+	argv <- getArgs
+	print argv
+	(opts, fname) <- compilerOpts argv
+	print opts 
+	print fname
+	when (optShowVersion opts) $ do
+		putStrLn "VERSION"
+		exitSuccess
