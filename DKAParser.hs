f43738b7bfc4e5e9aaf804701abbce576a32ae14
Adam Bezak <bezoadam95@gmail.com>
2018-03-03 20:18:54 +0100

    add comments, init README

--- DKAParser.hs
+++ DKAParser.hs
@@ -28,12 +28,13 @@ checkStatesFormat states = all isInteger $ states
 
 -- Kontrola spravneho formatu pociatocneho stavu
 checkStartState :: [String] -> Bool
-checkStartState startState = if ((length $ startState) == 1)
-								then do 
-									let first = startState !! 0
-									if (isInteger $ first) then True
-										else False 
-								else False
+checkStartState startState = 
+	if ((length $ startState) == 1)
+		then do 
+			let first = startState !! 0
+			if (isInteger $ first) then True
+				else False 
+		else False
 
 -- Kontrola ci dane stavy sa nachadzaju vo vsetkych stavoch
 checkIfSublist :: ([String], [String]) -> Bool
@@ -46,39 +47,39 @@ removeItemFromList a list = [x | x <- list, x /= a]
 -- Predikat na overenie ci je pravidlo v spravnom tvare
 checkRuleFormat :: [String] -> String -> Bool
 checkRuleFormat allStates rule = do
-							let ruleList = removeItemFromList "," $ wordsWhen (==',') rule
-							if (((length $ ruleList) == 3) && ((length $ ruleList !! 1) == 1))
-								then do
-									let startState = ruleList !! 0
-									let symbol = head $ ruleList !! 1
-									let endState = ruleList !! 2
-									if (isInteger startState && isInteger endState && isAsciiLower symbol)
-										then do
-											if (elem startState allStates &&  elem endState allStates) then True
-												else False
-										else False
-							else False
+	let ruleList = removeItemFromList "," $ wordsWhen (==',') rule
+	if (((length $ ruleList) == 3) && ((length $ ruleList !! 1) == 1))
+		then do
+			let startState = ruleList !! 0
+			let symbol = head $ ruleList !! 1
+			let endState = ruleList !! 2
+			if (isInteger startState && isInteger endState && isAsciiLower symbol)
+				then do
+					if (elem startState allStates &&  elem endState allStates) then True
+						else False
+				else False
+	else False
 
 
 -- Skontroluje spravnost vsetkych stavov
 checkRules :: ([String], [String]) -> Bool
 checkRules (rules, allStatesList) = do
-							if (length rules /= 0) then do 
-								let checkRulePredicate = checkRuleFormat allStatesList
-								all (checkRulePredicate) rules
-							else False
+	if (length rules /= 0) then do 
+		let checkRulePredicate = checkRuleFormat allStatesList
+		all (checkRulePredicate) rules
+	else False
 
 -- Vytvorenie struktury automatu
 loadAutomatData :: ([String], [String], [String], [String]) -> Maybe Automat
 loadAutomatData (allStatesList, startStateList, endStatesList, rules) = do
-																if (checkStatesFormat allStatesList && checkStartState startStateList && 
-																	checkStatesFormat endStatesList && checkIfSublist (startStateList, allStatesList) && 
-																	checkIfSublist (endStatesList, allStatesList) && checkRules (rules, allStatesList)) then do
-																		let transitions = map makeTransition rules
-																		Just Automat { 	states = map (read::String->State) allStatesList,
-																					delta = map makeTransition rules,
-																					sigma = getSigma transitions,
-																					initialState = read (startStateList !! 0) :: State,
-																					endStates = map (read::String->State) endStatesList
-																				}
-																else Nothing
+	if (checkStatesFormat allStatesList && checkStartState startStateList && 
+		checkStatesFormat endStatesList && checkIfSublist (startStateList, allStatesList) && 
+		checkIfSublist (endStatesList, allStatesList) && checkRules (rules, allStatesList)) then do
+			let transitions = map makeTransition rules
+			Just Automat { 	states = map (read::String->State) allStatesList,
+						delta = map makeTransition rules,
+						sigma = getSigma transitions,
+						initialState = read (startStateList !! 0) :: State,
+						endStates = map (read::String->State) endStatesList
+					}
+	else Nothing
